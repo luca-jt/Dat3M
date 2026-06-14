a25c9c0bcd9dbab6a0679536d04a3da8d8168562
Luca Thomas <random.lulu@die-genossen.de>
2026-06-14 21:09:27 +0200

    stuff

---
+++ benchmarks/luca/manyRegReads2_hard.c
@@ -0,0 +1,30 @@
+#include <assert.h>
+#include <dat3m.h>
+
+#define N 13
+
+static int global = 0;
+
+int main()
+{
+    int r1 = global;
+    for (int i = 0; i < N+1; i++) {
+        if (__VERIFIER_nondet_bool()) {
+            r1 += i;
+        }
+    }
+
+    //int temp = (r1 + r1) / 2;
+
+    int r2 = 0;
+    for (int i = 0; i < N; i++) {
+        if (__VERIFIER_nondet_bool()) {
+            r2 += r1;
+            //r2 += temp;
+        }
+    }
+
+    global = r2;
+
+    assert(global <= N*(N*(N+1))/2);
+}
